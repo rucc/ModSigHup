906d465390bae30e66c5e42a9b3cd5a0859ab946
rucc <harangoz@gmail.com>
2019-05-13 12:01:41 +0200

    Module init

---
+++ ModSigHup/ModSigHup.cs
@@ -0,0 +1,27 @@
+﻿using FreeSWITCH;
+using FreeSWITCH.Native;
+
+namespace ModSigHup
+{
+	public class ModSigHup : IApiPlugin
+	{
+		public void Execute(ApiContext context)
+		{
+			Execute();
+			context.Stream.Write($"ApiDemo executed");
+		}
+
+		public void ExecuteBackground(ApiBackgroundContext context)
+		{
+			Execute();
+			Log.WriteLine(LogLevel.Notice, $"ApiDemo executed");
+		}
+
+		public static void Execute()
+		{
+			var evt = new Event("TRAP", null);
+			evt.AddHeader("Trapped-Signal", "HUP");
+			evt.Fire();
+		}
+	}
+}
